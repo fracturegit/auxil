56b54156ee4a96365ee7b919dd56a629da9f0e83
Daniel Orr <orrdaniel73@gmail.com>
2026-02-05 23:04:45 +0000

    feat: gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,59 @@
 @file:Suppress("PropertyName")
 
-val brawls_api_version: String by properties
-val jaudiotagger_version: String by properties
+plugins {
+    kotlin("jvm") version("2.3.0")
+    id("java")
+    id("maven-publish")
+}
+
+group = "net.mcbrawls"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io/")
+    maven("https://maven.mcbrawls.net/releases/")
+    maven("https://libraries.minecraft.net/")
+}
 
 dependencies {
     api("net.kyori:adventure-key:4.25.0")
-    api("net.mcbrawls:brawls-api:$brawls_api_version")
-    api("net.jthink:jaudiotagger:$jaudiotagger_version")
-    api(project(":codex"))
+    api("net.mcbrawls.api:core:1.1.0")
+    api("net.mcbrawls:codex:2.0.0")
+    api("net.jthink:jaudiotagger:3.0.1")
+    api("com.github.mgrzeszczak:json-dsl:v1.1")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+        }
+    }
+
+    repositories {
+        val mavenUrl = System.getenv("MAVEN_URL")
+        if (mavenUrl != null) {
+            maven {
+                name = "envmaven"
+                url = uri(mavenUrl)
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
+    }
 }
